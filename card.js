c3156007f247c9a4b11a82726171414e63f19b07
Brian-X <brian@bae-media.com>
2017-09-10 23:44:18 -0500

    more goobering
    not working.  no idea what I'm suppose to be doing with it.

---
+++ card.js
@@ -0,0 +1,10 @@
+var BasicFlashcard = require('./basic-flashcard.js');
+
+
+var ClozeFlashcard = require('./cloze-flashcard.js');
+
+
+var inquirer = require('inquirer');
+
+
+var fs = require('fs');
